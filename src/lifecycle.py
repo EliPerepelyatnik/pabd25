7fb423402541e13bea81c8ea86bb79f1c21039a2
EliPerepelyatnik <elizaveta.perepelyatnik@yandex.ru>
2025-05-07 21:46:38 +0300

    seminar 07052025

---
+++ src/lifecycle.py
@@ -0,0 +1,136 @@
+import datetime
+import cianparser
+import pandas as pd
+import os
+import glob 
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+
+"""  Parse data from cian.ru
+https://github.com/lenarsaitov/cianparser
+"""
+moscow_parser = cianparser.CianParser(location="Москва")
+
+
+def parse_cian():
+    """
+    Function docstring
+    """
+    
+    n_rooms = 1
+    csv_path = f'data/raw/{n_rooms}_flats.csv'
+    data = moscow_parser.get_flats(
+        deal_type="sale",
+        rooms=(n_rooms,),
+        with_saving_csv=False,
+        additional_settings={
+            "start_page": 1,
+            "end_page": 1,
+            "object_type": "secondary"
+        })
+    df_new = pd.DataFrame(data)
+
+    df_new['parse_date'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    
+    if os.path.exists(csv_path):
+        df_existing = pd.read_csv(csv_path)
+        df_combined = pd.concat([df_existing, df_new]).drop_duplicates()
+        df_combined.to_csv(csv_path, encoding='utf-8', index=False)
+    else:
+        df_new.to_csv(csv_path, encoding='utf-8', index=False)
+
+
+def preprocess_data():
+  
+    raw_data_path = 'data/raw'
+    file_list = glob.glob(raw_data_path + "/*.csv")
+    cwd = os.getcwd()
+    print(cwd)
+    print(file_list) 
+
+    df = pd.read_csv(file_list[0])
+    df.dropna()
+    
+    new_df = df[df['total_meters'] < 300]
+
+    os.makedirs('../data/processed', exist_ok=True)
+    df.to_csv('../data/processed/data_cleaned.csv', encoding='utf-8')
+
+def train_test_model(model_name):
+    new_df = pd.read_csv("../data/processed/data_cleaned.csv")
+    new_df["flat_id"] = new_df["url"].str.extract(r"/flat/(\d+)/")[0].astype("int64")
+
+    test_size = int(len(new_df["flat_id"]) * 0.2)
+    test_items = sorted(new_df["flat_id"])[-test_size:]
+
+    train_data = new_df[~new_df["flat_id"].isin(test_items)]
+    test_data = new_df[new_df["flat_id"].isin(test_items)]
+    
+    X_train = train_data[["total_meters"]]  # только один признак - площадь
+    y_train = train_data["price"]
+
+    X_test = test_data[["total_meters"]]  # только один признак - площадь
+    y_test = test_data["price"]
+
+    # Создание и обучение модели
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Предсказание на тестовой выборке
+    y_pred = model.predict(X_test)
+
+    # Оценка модели
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+
+    # Вывод метрик качества
+    # todo: use logging
+    print(f"Среднеквадратичная ошибка (MSE): {mse:.2f}")
+    print(f"Корень из среднеквадратичной ошибки (RMSE): {rmse:.2f}")
+    print(f"Коэффициент детерминации R²: {r2:.6f}")
+    print(f"Средняя ошибка предсказания: {np.mean(np.abs(y_test - y_pred)):.2f} рублей")
+
+    # Коэффициенты модели
+    print(f"Коэффициент при площади: {model.coef_[0]:.2f}")
+    print(f"Свободный член: {model.intercept_:.2f}")
+
+    user_input = float(input('введите число метров'))
+    input_df = pd.DataFrame({"total_meters":[user_input]})
+    predicted = model.predict(input_df)[0]
+    print(f"{predicted:,.0f}")
+
+def save_model(model, model_name):
+    os.makedirs('models', exist_ok=True)
+    model_path = f'models/{model_name}.pkl'
+    joblib.dump(model, model_path)
+    print(f"Модель сохранена как: {model_path}")
+    return model_path
+    
+    #logging.info(f"Модель сохранена: {model_path}")
+
+    
+def main():
+    parser = argparse.ArgumentParser(description="Предсказание цен на жилье")
+    parser.add_argument('--model', type=str, default="linear_regression_model",
+                      help="Имя для сохранения модели")
+    args = parser.parse_args()
+
+    print("1. Парсинг данных...")
+    parse_cian()
+    
+    print("2. Предобработка данных...")
+    preprocess_data()
+    
+    print("3. Обучение модели...")
+    train_test_model(model_name=args.model)
+
+if __name__ == "__main__":
+    main()
+
