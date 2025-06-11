2a28c8425e6178aef54ce850d137b20e5ee9f837
EliPerepelyatnik <elizaveta.perepelyatnik@yandex.ru>
2025-06-11 21:52:31 +0300

    stage 8

--- service/app.py
+++ service/app.py
@@ -2,6 +2,26 @@ import logging
 import joblib
 from flask import Flask, jsonify, render_template, request
 from pydantic import BaseModel, ValidationError, field_validator
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from flask_httpauth import HTTPTokenAuth
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+import io
+
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_S3_BUCKET = os.getenv("BUCKET_NAME")
+AWS_S3_MODEL_KEY = os.getenv("LOCAL_FILE_PATH")
+CORRECT_TOKEN=os.getenv("API_TOKEN")
 
 
 class Apartment(BaseModel):
@@ -9,6 +29,7 @@ class Apartment(BaseModel):
     num_rooms: int
     total_floors: int
     floor: int
+    token: str
 
     @field_validator('area', 'num_rooms', 'total_floors', 'floor', mode='after')
     @classmethod
@@ -21,7 +42,47 @@ class Apartment(BaseModel):
 
 
 app = Flask(__name__)
-model = joblib.load('models/gbr_model_v15.pkl')
+CORS(app)
+auth = HTTPTokenAuth(scheme="Bearer")
+
+@auth.verify_token
+def verify_token(token):
+    return token == CORRECT_TOKEN
+
+
+def load_model_from_s3():
+    try:
+        s3 = boto3.client(
+            's3',
+            endpoint_url='https://storage.yandexcloud.net',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+        
+        # Получаем объект из S3
+        response = s3.get_object(Bucket=AWS_S3_BUCKET, Key='perepelyatnik/models/gbr_model_v15.pkl')
+        
+        return joblib.load(io.BytesIO(response['Body'].read()))
+        
+           
+    except NoCredentialsError:
+        logger.error("AWS credentials not found")
+        raise
+    except ClientError as e:
+        logger.error(f"S3 error: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Ошибка загрузки модели: {e}")
+        raise
+
+
+# Инициализация модели при старте приложения
+try:
+    model = load_model_from_s3()
+    logger.info("Модель успешно загружена из S3")
+except Exception as e:
+    logger.critical(f"Не удалось загрузить модель: {e}")
+    exit(1)
 
 # Маршрут для отображения формы
 @app.route('/')
@@ -38,6 +99,10 @@ def process_numbers():
         num_rooms = apartment.num_rooms
         total_floors = apartment.total_floors
         floor = apartment.floor
+
+        if apartment.token != CORRECT_TOKEN:
+            logger.warning("Неверный токен")
+            return jsonify({"error": "Неверный токен"}), 403
 
         prediction = model.predict([[area, num_rooms, total_floors, floor]])
         pretty_prediction = round((prediction[0] / 1_000_000), 2)
---
+++ src/upload_to_s3.py
@@ -0,0 +1,32 @@
+import argparse
+
+from dotenv import dotenv_values
+import boto3
+
+BUCKET_NAME = 'pabd25'
+YOUR_SURNAME = 'perepelyatnik'
+LOCAL_FILE_PATH = ['models/gbr_model_v15.pkl']
+
+config = dotenv_values(".env")
+
+
+def main(args):
+    client = boto3.client(
+        's3',
+        endpoint_url='https://storage.yandexcloud.net',
+        aws_access_key_id=config['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY']
+    )
+
+    for file_path in args.input:
+        object_name = f'{YOUR_SURNAME}/' + file_path.replace('\\', '/')
+        client.upload_file(file_path, BUCKET_NAME, object_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', nargs='+',
+                        help='Input local data files to upload to S3 storage',
+                        default=LOCAL_FILE_PATH)
+    args = parser.parse_args()
+    main(args)
